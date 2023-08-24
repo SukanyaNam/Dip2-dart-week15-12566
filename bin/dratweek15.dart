971d84b36ea2a51a17c01f65a527ee4881b611d4
SukanyaNam <kjjj5406@gmail.com>
2023-08-24 15:40:36 +0700

    first commit

---
+++ bin/dratweek15.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:dratweek15/list_demo.dart';
+import 'package:dratweek15/sub_program.dart';
+
+void main (){
+  basic();
+}
+
+void basic(){
+  mapDemo();
+  //listDemo();
+  //dowhile2();
+  //dowhile();
+  //whileloop();
+}
---
+++ test/dratweek15_test.dart
@@ -0,0 +1,8 @@
+import 'package:dratweek15/dratweek15.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
